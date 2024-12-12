43be5746a52465d6b0be07d792f19936a161c444
FuthifoXx <futhifoxmaseko@gmail.com>
2024-12-12 12:41:23 +0200

    installed cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const userRoutes = require('./routes/userRoute');
 const bookRoutes = require('./routes/bookRoute');
 const shopRoutes = require('./routes/shopRoute');
@@ -9,6 +10,11 @@ const live_uri =
   'mongodb+srv://futhifoxmaseko:FuthiFoXx_54140@cluster0.y7f3g.mongodb.net/myStore?retryWrites=true&w=majority&appName=Cluster0';
 //VeMnaP68YmymgWti
 
+app.get('/', (req, res) => {
+  res.send('api is ready!!!');
+});
+
+app.use(cors());
 app.use(express.json());
 app.use('/auth', userRoutes);
 app.use('/book', bookRoutes);
